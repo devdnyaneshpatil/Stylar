c73f4e62c7d344746e475cccd490adfc6052b70b
Dnyaneshwar <dnyaneshwarpatil830831@gmail.com>
2024-02-22 17:41:14 +0530

    updated user model

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,10 +1,31 @@
 const mongoose=require("mongoose")
 
-const userSchema=mongoose.Schema({
-    name:String,
-    email:String,
-    password:String
-})
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role:{
+    type:String,
+    enum:["servicer","user"],
+    default:"user"
+  },
+  shopName:{
+    type:String,
+    default:null
+  },
+  mobile:String,
+  age:Number
+});
 
 const UserModel=mongoose.model('user',userSchema)
 
